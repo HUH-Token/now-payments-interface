91cb313e29189cc4765e6019b7c3a24146b79f30
DsAtHuH <ds@huh.social>
2021-12-15 19:06:13 +0000

    :construction: Development path.

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-curl --location --request GET 'https://api.nowpayments.io/v1/payment/?limit=10&page=0&sortBy=created_at&orderBy=asc&dateFrom=2020-01-01&dateTo=2021-01-01' \
+endpoint="https://api.nowpayments.io/v1/payment/"
+limit="?limit=500"
+# sort="&sortBy=created_at"
+sort="&sortBy=pay_currency"
+order="&orderBy=asc"
+dateFrom="&dateFrom=2020-01-01"
+dateTo="&dateTo=2021-12-15"
+page="&page=0"
+curl --location --request GET "$endpoint$limit$page$order$dateFrom$dateTo$sort" \
 --header "x-api-key: $NOW_PAYMENTS_API_KEY"
